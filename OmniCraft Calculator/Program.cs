fe621a75cbbaefe29873891fa290ee78ad10fdc9
skomanex <volland.jimmy@gmail.com>
2025-05-01 15:49:34 +0200

    Ajouté l'analyse des Data Source pour offrir la possibilité d'avoir différentes liste de recettes de manière non destructive.

--- OmniCraft Calculator/Program.cs
+++ OmniCraft Calculator/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bienvenu dans l'Omnicrafter de Jimmy");
+
+        // Trouve les fichiers qui commencent par "craftingtree" qui sont l'ensemble des listes de recettes (une par jeu)
+        Regex regexCraftingTree = new Regex(@"^craftingtree_", RegexOptions.IgnoreCase);
+        List<string> craftingTrees = Directory
+            .GetFiles("Data Sources")
+            .Where(file => regexCraftingTree.IsMatch(Path.GetFileName(file)))
+            .ToList();
+
+        // Affiche la totalités des listes de recettes trouvées
+        if (craftingTrees.Count == 0) {
+            Console.WriteLine("Aucune liste de recettes pour craft trouvee, veuillez en renseigner au moins une dans le dossier Data Sources");
+            Environment.Exit(1);
+        }
+        else
+        {
+            Console.WriteLine($"{craftingTrees.Count} liste(s) de recettes ont été trouvee(s):");
+            foreach (var craftingTree in craftingTrees.Select((value, index) => (value, index)))
+            {
+                Console.WriteLine($"{craftingTree.index + 1}. {Path.GetFileName(craftingTree.value)}");
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
