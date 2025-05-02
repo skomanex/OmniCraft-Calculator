a1ff6b2c4452563515cdc0fc06799491e255e0ab
skomanex <volland.jimmy@gmail.com>
2025-05-03 01:33:53 +0200

    Compartementalisation du code pour lisbilite de la fonction main, ajouter selection de l'arbre de recettes et recherche des listes de craft associees

--- OmniCraft Calculator/Program.cs
+++ OmniCraft Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Xml.Schema;
 
@@ -8,27 +9,78 @@ public class Program
     {
         Console.WriteLine("Bienvenu dans l'Omnicrafter de Jimmy");
 
-        // Trouve les fichiers qui commencent par "craftingtree" qui sont l'ensemble des listes de recettes (une par jeu)
-        Regex regexCraftingTree = new Regex(@"^craftingtree_", RegexOptions.IgnoreCase);
+        List<string> craftingTrees = FindCraftingTrees();
+
+        Console.WriteLine($"{craftingTrees.Count} arbres(s) de recettes trouve(s):");
+        foreach (var craftingTree in craftingTrees.Select((value, index) => (value, index)))
+        {
+            Console.WriteLine($"{craftingTree.index}. {Path.GetFileName(craftingTree.value)}");
+        }
+
+        string selectedTree = SelectCraftingTree(craftingTrees);
+
+        List<string> craftingLists = FindCraftingLists(selectedTree);
+
+        Console.WriteLine($"{craftingLists.Count} liste(s) de craft trouvee(s) pour l'arbre de recettes specifie:");
+        foreach (var craftingList in craftingLists.Select((value, index) => (value, index)))
+        {
+            Console.WriteLine($"{craftingList.index}. {Path.GetFileName(craftingList.value)}");
+        }
+
+
+        Console.ReadLine();
+
+    }
+
+    static List<string> FindCraftingTrees()
+    {
+        Regex regexCraftingTree = new Regex(@"^craftingtree_.*\.json$", RegexOptions.IgnoreCase);
         List<string> craftingTrees = Directory
             .GetFiles("Data Sources")
             .Where(file => regexCraftingTree.IsMatch(Path.GetFileName(file)))
             .ToList();
 
-        // Affiche la totalités des listes de recettes trouvées
-        if (craftingTrees.Count == 0) {
-            Console.WriteLine("Aucune liste de recettes pour craft trouvee, veuillez en renseigner au moins une dans le dossier Data Sources");
+        if (craftingTrees.Count == 0)
+        {
+            Console.WriteLine("Aucun arbre de recettes pour craft trouve, veuillez en renseigner au moins un dans le dossier Data Sources");
             Environment.Exit(1);
+        }
+
+        return craftingTrees;
+    }
+
+    static string SelectCraftingTree(List<string> craftingTrees)
+    {
+        Console.WriteLine($"Veuillez choisir un arbre de craft (entrer un nombre entre 0 et {craftingTrees.Count - 1})");
+        string selectedTree = "";
+        if (int.TryParse(Console.ReadLine(), out int choice) && choice >= 0 && choice <= craftingTrees.Count)
+        {
+            selectedTree = craftingTrees[choice];
+            Console.WriteLine($"Vous avez selectionne : {Path.GetFileName(selectedTree)}");
         }
         else
         {
-            Console.WriteLine($"{craftingTrees.Count} liste(s) de recettes ont été trouvee(s):");
-            foreach (var craftingTree in craftingTrees.Select((value, index) => (value, index)))
-            {
-                Console.WriteLine($"{craftingTree.index + 1}. {Path.GetFileName(craftingTree.value)}");
-            }
+            Console.WriteLine("Entree utilisateur erronee");
+            SelectCraftingTree(craftingTrees);
         }
-        Console.ReadLine();
+        return selectedTree;
+    }
+
+    static List<string> FindCraftingLists(string selectedTree)
+    {
+        string gameName = Path.GetFileNameWithoutExtension(selectedTree).Replace("craftingtree_","");
+        Regex regexCraftingList = new Regex($@"^craftinglist_{gameName}_[0-9]*\.json$", RegexOptions.IgnoreCase);
+        List<string> craftingLists = Directory
+            .GetFiles("Data Sources")
+            .Where(file => regexCraftingList.IsMatch(Path.GetFileName(file)))
+            .ToList();
+        if (craftingLists.Count == 0)
+        {
+            Console.WriteLine("Aucune liste de craft trouvee pour l'arbre de recettes specifie, souhaitez-vous creer une liste a la volee? (Y/N):\n");
+            Console.ReadLine();
+        }
+
+        return craftingLists;
     }
 }
 
